a6ff8ef69b2850ff314f189661d8e7a6f5cc8063
mdevans <michael@dx.life>
2023-05-12 15:44:10 +0200

    v1.0.1 updater test

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const notification = document.getElementById('notification');
 const message = document.getElementById('message');
 const restartButton = document.getElementById('restart-button');
 
-ipcRenderer.on('update_available', () => {
-  ipcRenderer.removeAllListeners('update_available');
+electron.on('update_available', () => {
+  electron.removeAllListeners('update_available');
   message.innerText = 'A new update is available. Downloading now...';
   notification.classList.remove('hidden');
 });
 
-ipcRenderer.on('update_downloaded', () => {
-  ipcRenderer.removeAllListeners('update_downloaded');
+electron.on('update_downloaded', () => {
+  electron.removeAllListeners('update_downloaded');
   message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
   restartButton.classList.remove('hidden');
   notification.classList.remove('hidden');
@@ -27,5 +27,5 @@ function closeNotification() {
   notification.classList.add('hidden');
 }
 function restartApp() {
-  ipcRenderer.send('restart_app');
+  electron.send('restart_app');
 }
